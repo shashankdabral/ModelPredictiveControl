0267d27109e5f79a21dde94898aada51a3853628
Shashank Dabral <a0393223@ti.com>
2018-01-31 23:35:57 -0600

    debug_1

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,10 @@ int main() {
           */
           double steer_value;
           double throttle_value;
-
+	  vector<double> solution = mpc.solve(state,coeffs);
+	  
+          steer_value =  -1.0 * soluiton[0];
+	  throttle_value = solution[1];
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].

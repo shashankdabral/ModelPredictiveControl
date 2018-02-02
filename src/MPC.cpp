9a2961a706ee54d1870f3650086ae7bd8150c39c
Shashank Dabral <a0393223@ti.com>
2018-02-01 22:16:42 -0600

    parameter tuning

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -48,7 +48,7 @@ class FG_eval {
     
     fg[0] =0 ; /* Define the cost function here */
     for (t=0;t<N;t++) {
-        fg[0] =+ 2000*  CppAD::pow(vars[cte_start +t],2); //Cross track error
+        fg[0] =+ 5000*  CppAD::pow(vars[cte_start +t],2); //Cross track error
 	fg[0] += 2000* CppAD::pow(vars[epsi_start+t],2); // psi error
 	fg[0] += CppAD::pow(vars[v_start+t]-ref_v,2);
     } // For int t
